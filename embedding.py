415da1c4e05a8942d6df725d1622eff7d70aeb76
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-10-01 15:33:50 +0900

    add:encoder decoderモデル

--- embedding.py
+++ embedding.py
@@ -2,33 +2,32 @@ import tensorflow as tf
 import math
 
 class AddPositionnalEncoding(tf.keras.layers.Layer):
-  '''
-  入力テンソルに対し、位置の情報を付与して返すレイヤーです。
-  
-  PE_{pos, 2i}   = sin(pos / 10000^{2i / d_model})
-  PE_{pos, 2i+1} = cos(pos / 10000^{2i / d_model})
-  '''
-  
-  def call(self, inputs: tf.Tensor) -> tf.Tensor:
-    fl_type = inputs.dtype
-    batch_size, max_length, depth = tf.unstack(tf.shape(inputs))
+    '''
+    入力テンソルに対し、位置の情報を付与して返すレイヤーです。
     
-    depth_counter = tf.range(depth) // 2 * 2  # 0, 0, 2, 2, 4, ...
-    depth_matrix = tf.tile(tf.expand_dims(depth_counter, 0), [max_length, 1])  # [max_length, depth]
-    depth_matrix = tf.pow(10000.0, tf.cast(depth_matrix / depth, fl_type))  # [max_length, depth]
-    
-    # cos(x) == sin(x + π/2)
-    phase = tf.cast(tf.range(depth) % 2, fl_type) * math.pi / 2  # 0, π/2, 0, π/2, ...
-    phase_matrix = tf.tile(tf.expand_dims(phase, 0), [max_length, 1])  # [max_length, depth]
-    
-    pos_counter = tf.range(max_length)
-    pos_matrix = tf.cast(tf.tile(tf.expand_dims(pos_counter, 1), [1, depth]), fl_type)  # [max_length, depth]
-    
-    positional_encoding = tf.sin(pos_matrix / depth_matrix + phase_matrix)
-    # [batch_size, max_length, depth]
-    positional_encoding = tf.tile(tf.expand_dims(positional_encoding, 0), [batch_size, 1, 1])
-    
-    return inputs + positional_encoding
+    PE_{pos, 2i}   = sin(pos / 10000^{2i / d_model})
+    PE_{pos, 2i+1} = cos(pos / 10000^{2i / d_model})
+    '''
+    def call(self, inputs: tf.Tensor) -> tf.Tensor:
+        fl_type = inputs.dtype
+        batch_size, max_length, depth = tf.unstack(tf.shape(inputs))
+        
+        depth_counter = tf.range(depth) // 2 * 2  # 0, 0, 2, 2, 4, ...
+        depth_matrix = tf.tile(tf.expand_dims(depth_counter, 0), [max_length, 1])  # [max_length, depth]
+        depth_matrix = tf.pow(10000.0, tf.cast(depth_matrix / depth, fl_type))  # [max_length, depth]
+        
+        # cos(x) == sin(x + π/2)
+        phase = tf.cast(tf.range(depth) % 2, fl_type) * math.pi / 2  # 0, π/2, 0, π/2, ...
+        phase_matrix = tf.tile(tf.expand_dims(phase, 0), [max_length, 1])  # [max_length, depth]
+        
+        pos_counter = tf.range(max_length)
+        pos_matrix = tf.cast(tf.tile(tf.expand_dims(pos_counter, 1), [1, depth]), fl_type)  # [max_length, depth]
+        
+        positional_encoding = tf.sin(pos_matrix / depth_matrix + phase_matrix)
+        # [batch_size, max_length, depth]
+        positional_encoding = tf.tile(tf.expand_dims(positional_encoding, 0), [batch_size, 1, 1])
+        
+        return inputs + positional_encoding
 
 PAD_ID = 0
 
